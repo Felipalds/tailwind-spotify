f9d2ae64f1e13f2ebcb2938d96c4b79ced6caedf
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2024-07-17 15:00:37 -0300

    spotify in tailwind

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+![](assets/spotify.png)
+
 First, run the development server:
 
 ```bash
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,184 @@
+import {
+  Home as HomeIcon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+  Repeat,
+  Mic2,
+  LayoutList,
+  Laptop2,
+  Volume,
+  Maximize,
+} from "lucide-react";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <nav className="space-y-5">
+            <a
+              href=""
+              className="flex items-center gap-2 text-xs font-semibold text-zinc-200"
+            >
+              <HomeIcon />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 text-xs font-semibold text-zinc-200"
+            >
+              <Search />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 text-xs font-semibold text-zinc-200"
+            >
+              <Library />
+              Your library
+            </a>
+          </nav>
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-2">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Chill Vibes
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Workout Hits
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Top 40
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Jazz Classics
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Indie Mix
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Throwback Jams
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Hip Hop Essentials
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Party Playlist
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Relax & Unwind
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Acoustic Favourites
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Rock Anthems
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6 overflow-y-auto">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            {Array.from({ length: 6 }).map((_, index) => (
+              <a
+                href="#"
+                key={index}
+                className="group bg-white/10 rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transitions-colors"
+              >
+                <Image
+                  src="/album.jpeg"
+                  alt="Spotify"
+                  width={104}
+                  height={104}
+                />
+                <strong>My Beautiful Dark Twisted Fantasy</strong>
+                <button className="p-2 rounded-full bg-green-500 text-black ml-auto mr-4 invisible group-hover:visible">
+                  <Play className="pl-0.2" />
+                </button>
+              </a>
+            ))}
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">Made for Luiz Felipe</h2>
+
+          <div className="grid grid-cols-5 gap-4 mt-4">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <a
+                href="#"
+                key={index}
+                className="bg-white/5 p-3 rounded hover:bg-white/10 flex flex-col gap-2"
+              >
+                <Image
+                  src="/album2.jpg"
+                  className="w-full"
+                  alt="Spotify"
+                  width={104}
+                  height={104}
+                />
+                <strong>Daily Mix 1</strong>
+                <span className="text-s text-zinc-500">
+                  Kanye West, Metallica, Matanza and more
+                </span>
+              </a>
+            ))}
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 items-center justify-between flex flex-row fixed bottom-0 w-full">
+        <div className="flex items-center gap-2">
+          <Image src="/album2.jpg" alt="Spotify" width={64} height={64} />
+          <div className="flex flex-col">
+            <strong className="font-normal">Good Morning</strong>
+            <span className="text-xs">Kanye West</span>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className=" text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="p-2 rounded-full bg-white text-black">
+              <Play className="pl-0.2" />
+            </button>
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} className="text-zinc-200" />
+          <LayoutList size={20} className="text-zinc-200" />
+          <Laptop2 size={20} className="text-zinc-200" />
+          <div className="flex items-center gap-2">
+            <Volume size={20} className="text-zinc-200" />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize size={20} className="text-zinc-200" />
+        </div>
+      </footer>
+    </div>
   );
 }
